84601479f56c1252f3f2106295ffca8e7cf4282a
Fireboltund <80182993+Fireboltund@users.noreply.github.com>
2021-05-28 05:12:48 +0800

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,15 @@
+# First & Last Name
+# Amirul Husnii
+# A fun fact about yourself
+# LOL WTF
+
+name = """
+   A        H     H
+ A  A       H     H
+A     A     H     H 
+ AAAAA      HHHHHHH
+A     A     H     H 
+A     A     H     H 
+A     A     H     H 
+"""
+print(name)
